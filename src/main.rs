ce1fca44fa7b2c6c25a322727b1385b1fbdfec9e
Alex Melville <Melvillian@users.noreply.github.com>
2020-04-03 12:09:48 -0400

    Get websocket connecting (#3)
    * get websockets connecting working as expected.
    
    I switched from socket.io to native Websockets after readining this helpful stack overflow post: https://stackoverflow.com/questions/19506735/client-side-socket-io-without-a-node-js-server
    
    * prettier test

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,6 @@
 use chipotle8::Interpreter;
 use futures::{FutureExt, StreamExt};
-use std::collections::HashMap;
-use std::sync::{
-    atomic::{AtomicUsize, Ordering},
-    Arc,
-};
-use tokio::sync::{mpsc, Mutex};
-use warp::ws::{Message, WebSocket};
 use warp::Filter;
-
-type Users = Arc<Mutex<HashMap<usize, mpsc::UnboundedSender<Result<Message, warp::Error>>>>>;
-
-/// Our global unique user id counter.
-static NEXT_USER_ID: AtomicUsize = AtomicUsize::new(1);
 
 /// the static HTML to serve
 static INDEX_HTML_PATH: &str = "public/index.html";
@@ -20,12 +8,6 @@ static INDEX_HTML_PATH: &str = "public/index.html";
 #[tokio::main]
 async fn main() {
     pretty_env_logger::init();
-
-    // Keep track of all connected users, key is usize, value
-    // is a websocket sender.
-    let users = Arc::new(Mutex::new(HashMap::new()));
-    // Turn our "state" into a new Filter...
-    let users = warp::any().map(move || users.clone());
 
     // GET / -> index html
     let index = warp::get()
@@ -39,92 +21,26 @@ async fn main() {
     let bundle = warp::path("public").and(warp::fs::dir("public"));
 
     // GET /chat -> websocket upgrade
-    let chat = warp::path("chat")
-        // The `ws()` filter will prepare Websocket handshake...
+    let chat = warp::path("echo")
+        // The `ws()` filter will prepare the Websocket handshake.
         .and(warp::ws())
-        .and(users)
-        .map(|ws: warp::ws::Ws, users| {
-            // This will call our function if the handshake succeeds.
-            ws.on_upgrade(move |socket| user_connected(socket, users))
+        .map(|ws: warp::ws::Ws| {
+            // And then our closure will be called when it completes...
+            ws.on_upgrade(|websocket| {
+                // Just echo all messages back...
+                let (tx, rx) = websocket.split();
+
+                let interpreter = Interpreter::with_game_file("data/PONG");
+
+                rx.forward(tx).map(|result| {
+                    if let Err(e) = result {
+                        eprintln!("websocket error: {:?}", e);
+                    }
+                })
+            })
         });
 
     let routes = index.or(chat).or(bundle);
 
     warp::serve(routes).run(([127, 0, 0, 1], 3000)).await;
-}
-async fn user_connected(ws: WebSocket, users: Users) {
-    // Use a counter to assign a new unique ID for this user.
-    let my_id = NEXT_USER_ID.fetch_add(1, Ordering::Relaxed);
-
-    eprintln!("new chat user: {}", my_id);
-
-    // Split the socket into a sender and receive of messages.
-    let (user_ws_tx, mut user_ws_rx) = ws.split();
-
-    // Use an unbounded channel to handle buffering and flushing of messages
-    // to the websocket...
-    let (tx, rx) = mpsc::unbounded_channel();
-    tokio::task::spawn(rx.forward(user_ws_tx).map(|result| {
-        if let Err(e) = result {
-            eprintln!("websocket send error: {}", e);
-        }
-    }));
-
-    // Save the sender in our list of connected users.
-    users.lock().await.insert(my_id, tx);
-
-    // Return a `Future` that is basically a state machine managing
-    // this specific user's connection.
-
-    // Make an extra clone to give to our disconnection handler...
-    let users2 = users.clone();
-
-    // Every time the user sends a message, broadcast it to
-    // all other users...
-    while let Some(result) = user_ws_rx.next().await {
-        let msg = match result {
-            Ok(msg) => msg,
-            Err(e) => {
-                eprintln!("websocket error(uid={}): {}", my_id, e);
-                break;
-            }
-        };
-
-        let mut interpreter = Interpreter::with_game_file("data/PONG");
-        user_message(my_id, msg, &users).await;
-    }
-
-    // user_ws_rx stream will keep processing as long as the user stays
-    // connected. Once they disconnect, then...
-    user_disconnected(my_id, &users2).await;
-}
-
-async fn user_message(my_id: usize, msg: Message, users: &Users) {
-    // Skip any non-Text messages...
-    let msg = if let Ok(s) = msg.to_str() {
-        s
-    } else {
-        return;
-    };
-
-    let new_msg = format!("<User#{}>: {}", my_id, msg);
-
-    // New message from this user, send it to everyone else (except same uid)...
-    //
-    // We use `retain` instead of a for loop so that we can reap any user that
-    // appears to have disconnected.
-    for (&uid, tx) in users.lock().await.iter_mut() {
-        if let Err(_disconnected) = tx.send(Ok(Message::text(new_msg.clone()))) {
-            // The tx is disconnected, our `user_disconnected` code
-            // should be happening in another task, nothing more to
-            // do here.
-        }
-    }
-}
-
-async fn user_disconnected(my_id: usize, users: &Users) {
-    eprintln!("good bye user: {}", my_id);
-
-    // Stream closed up, so remove from the user list
-    users.lock().await.remove(&my_id);
 }
