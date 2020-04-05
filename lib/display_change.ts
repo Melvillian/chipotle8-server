3d458de13580284bcfc1ff635806ddc391be6cd2
Melvillian <melvillevt@gmail.com>
2020-04-05 11:15:12 -0400

    address comments

--- lib/display_change.ts
+++ lib/display_change.ts
@@ -1,5 +1,5 @@
 /**
- * An objects representing the pixel state for the pixel as x and y coordinates, where "true" is white and "false" is black.
+ * DisplayChange represents the pixel state for the pixel at x and y coordinates, where "true" is white and "false" is black.
  *
  * For example, we might have `let change: DisplayChange = { x: 0, x: 0, isAlive: true };` for a white pixel in the top left of the display
  */
