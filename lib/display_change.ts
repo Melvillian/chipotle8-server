b1f3e603ebde19d7b37850948e94198314de3a64
Alex Melville <Melvillian@users.noreply.github.com>
2020-04-05 11:15:42 -0400

    Add frontend canvas (#5)
    * add 2d canvas for displaying interpreter's display.
    
    However this only sets up the display, it doesn't actually run the interpreter and push those changes to the client. That will come later
    
    * use object instead of tuple so it can be serialized easier
    
    * address comments

--- lib/display_change.ts
+++ lib/display_change.ts
@@ -1,8 +1,8 @@
 /**
- * A tupe representing the pixel state for the pixel as x and y coordinates, where "true" is white and "false" is black.
+ * DisplayChange represents the pixel state for the pixel at x and y coordinates, where "true" is white and "false" is black.
  *
- * For example, we might have `let change: DisplayChange = [0, 0, true];` for a white pixel in the top left of the display
+ * For example, we might have `let change: DisplayChange = { x: 0, x: 0, isAlive: true };` for a white pixel in the top left of the display
  */
-type DisplayChange = [number, number, boolean];
+type DisplayChange = { x: number; y: number; isAlive: boolean };
 
 export { DisplayChange };
--- lib/index.ts
+++ lib/index.ts
@@ -1,10 +1,84 @@
 import {
   parseServerMsg,
-  Message,
   MessageType,
   DisconnectMessage,
   DisplayChangeMessage,
 } from "./messaging";
+import { DisplayChange } from "./display_change";
+
+const CELL_SIZE = 10; // px
+const GRID_COLOR = "#CCCCCC";
+const DEAD_COLOR = "#FFFFFF";
+const ALIVE_COLOR = "#000000";
+const width = 64;
+const height = 32;
+
+// initialize display and fill with empty pixels
+const display = new Array(width * height);
+for (let i = 0; i < width * height; i++) {
+  display.push(DEAD_COLOR);
+}
+
+// Give the canvas room for all of our cells and a 1px border
+// around each of them.
+const canvas: HTMLCanvasElement = document.getElementById(
+  "game-of-life-canvas"
+) as HTMLCanvasElement;
+canvas.height = (CELL_SIZE + 1) * height + 1;
+canvas.width = (CELL_SIZE + 1) * width + 1;
+
+const ctx = canvas.getContext("2d");
+
+const renderLoop = () => {
+  drawGrid();
+  drawCells();
+
+  requestAnimationFrame(renderLoop);
+};
+
+const drawGrid = () => {
+  ctx.beginPath();
+  ctx.strokeStyle = GRID_COLOR;
+
+  // Vertical lines.
+  for (let i = 0; i <= width; i++) {
+    ctx.moveTo(i * (CELL_SIZE + 1) + 1, 0);
+    ctx.lineTo(i * (CELL_SIZE + 1) + 1, (CELL_SIZE + 1) * height + 1);
+  }
+
+  // Horizontal lines.
+  for (let j = 0; j <= height; j++) {
+    ctx.moveTo(0, j * (CELL_SIZE + 1) + 1);
+    ctx.lineTo((CELL_SIZE + 1) * width + 1, j * (CELL_SIZE + 1) + 1);
+  }
+
+  ctx.stroke();
+};
+
+const getIndex = (row: number, column: number) => {
+  return row * width + column;
+};
+
+const drawCells = () => {
+  ctx.beginPath();
+
+  for (let row = 0; row < height; row++) {
+    for (let col = 0; col < width; col++) {
+      const idx = getIndex(row, col);
+
+      ctx.fillStyle = display[idx];
+
+      ctx.fillRect(
+        col * (CELL_SIZE + 1) + 1,
+        row * (CELL_SIZE + 1) + 1,
+        CELL_SIZE,
+        CELL_SIZE
+      );
+    }
+  }
+
+  ctx.stroke();
+};
 
 const socket = new WebSocket("ws://localhost:3000/echo");
 
@@ -25,5 +99,15 @@ function onMessage(event: MessageEvent) {
   } else if (parsed.type() === MessageType.DisplayChange) {
     console.log("received displaychange");
     const msg = parsed as DisplayChangeMessage;
+
+    const changes = msg.changes;
+
+    for (let change of changes) {
+      const { x, y, isAlive } = change;
+      const idx = getIndex(x, y);
+      display[idx] = isAlive ? ALIVE_COLOR : DEAD_COLOR;
+    }
   }
 }
+
+requestAnimationFrame(renderLoop);
