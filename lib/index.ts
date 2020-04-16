78c667f7a203610f88ab6c14efe807fdd6ccd65b
Melvillian <melvillevt@gmail.com>
2020-04-16 11:13:30 -0400

    refactor image manipulation functions into their own module

--- lib/index.ts
+++ lib/index.ts
@@ -1,84 +1,5 @@
 import MyWorker = require("worker-loader?name=[name].js!./worker");
-
-// a canvas RGBA value requires 4 bytes
-const NUM_BYTES_IN_RGBA = 4;
-
-type CanvasChange = { x: number; y: number };
-
-const getIndex = (x: number, y: number, width: number) => {
-  return (y * width + x) * NUM_BYTES_IN_RGBA;
-};
-
-// a Canvas ImageData object is a widthxheightx4 array where each pixel is represented by an RGBA value
-// of 4 numbers.
-const writeRGBValue = (
-  imageData: ImageData,
-  x: number,
-  y: number,
-  width: number,
-  isWhite: boolean
-) => {
-  // Get the pixel index
-  const pixelIndex = getIndex(x, y, width);
-
-  // Set the pixel data
-  const pixelValue = isWhite ? 255 : 0;
-
-  imageData.data[pixelIndex + 0] ^= pixelValue;
-  imageData.data[pixelIndex + 1] ^= pixelValue;
-  imageData.data[pixelIndex + 2] ^= pixelValue;
-  imageData.data[pixelIndex + 3] = 255; // alpha value is always maxed out, because 0 is fully transparant
-};
-
-// given a pixel change from the emulator, return the corresponding
-const getCanvasChangesFromChange = (
-  chipX: number,
-  chipY: number,
-  width: number,
-  widthMultiplier: number,
-  heightMultiplier: number
-): CanvasChange[] => {
-  const canvasChanges: CanvasChange[] = [];
-
-  const startingX = chipX * widthMultiplier;
-  const startingY = chipY * heightMultiplier * width;
-  console.log(`chipX: ${chipX}, chipY: ${chipY}`);
-  for (let w = 0; w < widthMultiplier; w++) {
-    for (let h = 0; h < heightMultiplier; h++) {
-      const x = startingX + w;
-      const y = startingY + h * width;
-      canvasChanges.push({ x, y });
-    }
-  }
-
-  console.log(`canvasChanges: ${JSON.stringify(canvasChanges)}`);
-  return canvasChanges;
-};
-
-// Update a display pixel with the change coming in from the game server. This will
-// change multiple pixels on the canvas because a single pixel on the emulator
-// display is represented by multiple pixels on the canvas, the exact number
-// of which is determined by widthMultiplier and heightMultiplier
-const updateImageData = (
-  imageData: ImageData,
-  change: any,
-  width: number,
-  widthMultiplier: number,
-  heightMultiplier: number
-) => {
-  const { x: chipX, y: chipY, isAlive } = change;
-
-  for (let canvasChange of getCanvasChangesFromChange(
-    chipX,
-    chipY,
-    width,
-    widthMultiplier,
-    heightMultiplier
-  )) {
-    const { x, y } = canvasChange;
-    writeRGBValue(imageData, x, y, width, isAlive);
-  }
-};
+import { updateImageData, initializeImage } from "./image";
 
 let worker = new MyWorker();
 
@@ -100,7 +21,7 @@ window.onload = function () {
   const height = CHIP_8_HEIGHT * heightMultiplier;
 
   // Create an ImageData object
-  let imageData = context?.createImageData(width, height);
+  let imageData = context?.createImageData(width, height)!;
 
   // the CHIP-8 display is a fixed width and height, but the canvas
   // width and height can change. DISPLAY_RATIO is a multiplier
@@ -123,11 +44,11 @@ window.onload = function () {
     if (shouldPrint) {
       console.log(`updating with change ${JSON.stringify(change)}`);
 
-      before = imageData!.data.slice();
+      before = imageData.data.slice();
     }
 
     updateImageData(
-      imageData!,
+      imageData,
       change,
       width,
       widthMultiplier,
@@ -136,28 +57,18 @@ window.onload = function () {
 
     if (shouldPrint) {
       shouldPrint = false;
-      for (let i = 0; i < imageData!.data.length; i++) {
-        if (imageData!.data[i] !== before[i]) {
+      for (let i = 0; i < imageData.data.length; i++) {
+        if (imageData.data[i] !== before[i]) {
           console.log(
-            `discrepancy. i: ${i}, before: ${before[i]}, after: ${
-              imageData!.data[i]
-            }`
+            `discrepancy. i: ${i}, before: ${before[i]}, after: ${imageData.data[i]}`
           );
         }
       }
     }
   };
 
-  // Create the initial black pixel map
-  function initializeImage() {
-    // Loop over all of the pixels
-    for (let x = 0; x < width; x++) {
-      for (let y = 0; y < height; y++) {
-        writeRGBValue(imageData!, x, y, width, false);
-      }
-    }
-  }
-  initializeImage();
+  // setup our black pixel canvas
+  initializeImage(imageData, width, height);
 
   // Main loop
   function main(tframe: number) {
@@ -165,7 +76,7 @@ window.onload = function () {
     window.requestAnimationFrame(main);
 
     // Draw the image data to the canvas
-    context?.putImageData(imageData!, 0, 0);
+    context?.putImageData(imageData, 0, 0);
   }
 
   // Call the main loop
