2f253667c7f3a8aabbfc780a7f33c11967180165
Melvillian <melvillevt@gmail.com>
2020-04-18 17:21:39 -0400

    get rid of background grid drawing in favor of more performance css background

--- frontend/lib/index.ts
+++ frontend/lib/index.ts
@@ -10,8 +10,8 @@ let worker = new MyWorker();
 
 const CELL_SIZE = 10; // px
 const GRID_COLOR = "#CCCCCC";
-const DEAD_COLOR = "#FFFFFF";
-const ALIVE_COLOR = "#000000";
+const DEAD_COLOR = "#000000";
+const ALIVE_COLOR = "#FFFFFF";
 const width = 64;
 const height = 32;
 
@@ -32,33 +32,9 @@ canvas.width = (CELL_SIZE + 1) * width + 1;
 const ctx = canvas.getContext("2d");
 
 const renderLoop = () => {
-  drawGrid();
   drawCells();
 
   requestAnimationFrame(renderLoop);
-};
-
-const drawGrid = () => {
-  if (ctx !== null) {
-    ctx.beginPath();
-    ctx.strokeStyle = GRID_COLOR;
-
-    // Vertical lines.
-    for (let i = 0; i <= width; i++) {
-      ctx.moveTo(i * (CELL_SIZE + 1) + 1, 0);
-      ctx.lineTo(i * (CELL_SIZE + 1) + 1, (CELL_SIZE + 1) * height + 1);
-    }
-
-    // Horizontal lines.
-    for (let j = 0; j <= height; j++) {
-      ctx.moveTo(0, j * (CELL_SIZE + 1) + 1);
-      ctx.lineTo((CELL_SIZE + 1) * width + 1, j * (CELL_SIZE + 1) + 1);
-    }
-
-    ctx.stroke();
-  } else {
-    console.error("failed to load 2d canvas context!");
-  }
 };
 
 const getIndex = (x: number, y: number) => {
